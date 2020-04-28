b010dafbb406b3f7f7c4f1c455487a61c401b2f3
Maja Francetic <francetic.maja@gmail.com>
2020-04-28 14:00:54 +0200

    Create a login-register screen with sign up and login functionality; display toasts to inform the user if the action was successful

--- app/src/main/java/com/example/parseproject/MainActivity.java
+++ app/src/main/java/com/example/parseproject/MainActivity.java
@@ -1,19 +1,91 @@
 package com.example.parseproject;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button signUpButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupLoginRegisterView();
+
         // checking how much the user has used the app
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    private void setupLoginRegisterView() {
+        usernameEditText = findViewById(R.id.username_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        signUpButton = findViewById(R.id.sign_up_button);
+        loginButton = findViewById(R.id.login_button);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpUser(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+            }
+        });
+    }
+
+    private void signUpUser(String username, String password) {
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(MainActivity.this, getString(R.string.sign_up_successful),
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, getString(R.string.sign_up_failed),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+            if (e == null && user != null) {
+                Toast.makeText(MainActivity.this, getString(R.string.login_successful),
+                        Toast.LENGTH_LONG).show();
+            } else if (e != null) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, getString(R.string.login_failed),
+                        Toast.LENGTH_LONG).show();
+            }
+            }
+        });
     }
 }
